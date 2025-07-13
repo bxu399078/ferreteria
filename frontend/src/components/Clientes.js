c2bf95e80942cfefa6fa2eee4a3212f432c8da75
bxu399078 <bxu399078@gmail.com>
2025-07-13 16:33:12 -0300

    1

--- frontend/src/components/Clientes.js
+++ frontend/src/components/Clientes.js
@@ -6,7 +6,8 @@ const Clientes = () => {
   const [clientes, setClientes] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/api/clientes')
+    //axios.get('http://localhost:3001/api/clientes')
+    axios.get(process.env.REACT_APP_API_URL +'/api/clientes')
       .then(response => {
         setClientes(response.data);
       })
