531263c83ab51d2e9353b0bb0b64931573baf07a
bxu399078 <bxu399078@gmail.com>
2025-08-31 12:03:57 -0300

    anueva base de datos

--- backend/index.js
+++ backend/index.js
@@ -16,21 +16,21 @@ app.use(express.json());
 
 //console.log(process.env.DATABASE_URl)
 
-/*  const pool = new Pool({
+ const pool = new Pool({
   connectionString: process.env.DATABASE_URl,
   ssl:{
     rejectUnauthorized: false
   }
-}); */ 
+});  
   
-
+/* 
  const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-}); 
+});  */
   
 
 app.get('/api/clientes', async (req, res) => {
